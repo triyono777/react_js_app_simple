3bcfdf5d30a89a143a68e943a2648fa9e7f9128d
Triyono <yono.tri@gmail.com>
2022-06-12 19:15:11 +0700

    add delete product

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import ProductList from "./components/ProductList";
 import {useState} from "react"
 function App() {
-const [products,  setProduct] = useState([
+const [products,  setProducts] = useState([
   {id:1,title:"nama product 1",price:90000},
   {id:2,title:"nama product 2",price:99918},
   {id:3,title:"nama product 3",price:15525},
@@ -10,10 +10,13 @@ const [products,  setProduct] = useState([
   {id:5,title:"nama product 5",price:15522},
 ])
 
-
+const deleteProduct = (productId)=>{
+  const newPoducts = products.filter(product => product.id !== productId);
+  setProducts(newPoducts);
+}
   return (
   <div>
-    <ProductList products={products}/>
+    <ProductList products={products} deleteProduct= {deleteProduct}/>
 
   </div>
   );
