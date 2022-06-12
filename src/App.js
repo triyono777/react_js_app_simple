ee80b51396c33210e38806746e9ce594b8f4f5de
Triyono <yono.tri@gmail.com>
2022-06-12 18:46:23 +0700

    add looping list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import Header from "./components/Header";
 import {useState} from "react"
 function App() {
-const [title, setTitle] = useState('Hello word');
-const changeTitle = ()=>{
-  setTitle("Title changed")
-}
+const [product,  setProduct] = useState([
+  {id:1,title:"nama product 1",price:90000},
+  {id:2,title:"nama product 2",price:99918},
+  {id:3,title:"nama product 3",price:15525},
+  {id:4,title:"nama product 4",price:37736},
+  {id:5,title:"nama product 5",price:15522},
+])
 
 
   return (
   <div>
-    <h1>{title}</h1>
-    <button onClick={changeTitle}>Click me</button>
+<ul>
+  {product.map((product)=>(
+    <li key={product.id}> {product.title} - {product.price}</li>
+  ))}
+</ul>
   </div>
   );
 }
